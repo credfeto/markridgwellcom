4b194e44fdff3139a4b585b80ce1126086d20221
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-12-21 17:28:17 +0000

    changed over to use webpack

---
+++ webpack.common.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new ManifestPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            favicon: 'src/img/favicon.ico',
+            template: "src/index.html",
+            compile: true,
+            inject: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[hash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /\.module.(s(a|c)ss)$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    },
+};
